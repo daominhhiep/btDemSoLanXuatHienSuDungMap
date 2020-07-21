e3a815a15e4de0de90d0a5ede0f589b9f0798c7b
admin <daominhhiep97@gmail.com>
2020-07-21 14:54:46 +0700

    [Bài tập] Đếm số lần xuất hiện của mỗi từ trong một chuỗi sử dụng Map

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,11 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        String[] arr = inputString();
-        Map<String, Integer> treeMap = checkKeyInMap(arr);
-        display(treeMap);
-    }
-
-    private static String[] inputString() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string : ");
         String string = scanner.nextLine();
-        return string.split(" ");
-    }
+        String[] arr = string.split(" ");
 
-    private static Map<String, Integer> checkKeyInMap(String[] arr) {
         Map<String, Integer> treeMap = new TreeMap<>();
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i].toLowerCase();
@@ -27,10 +19,6 @@ public class Main {
                 treeMap.put(arr[i], 1);
             }
         }
-        return treeMap;
-    }
-
-    private static void display(Map<String, Integer> treeMap) {
         for (Map.Entry<String, Integer> entry : treeMap.entrySet())
             System.out.println(entry.getKey() + " : " + entry.getValue());
     }
